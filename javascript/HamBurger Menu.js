d5e9d43a3e0f89940f521a67a95ae23a55fbcb54
Tom-BPC <10656047@student.bpc.ac.uk>
2023-01-06 21:30:10 +0000

    Revert "Add files via upload"
    This reverts commit 51fc06d81a4bac2ad481c75420612daba3012d72.

--- javascript/HamBurger Menu.js
+++
@@ -1,8 +0,0 @@
-function hamburgermenu() {
-    var x = document.getElementById("hamburgerlinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } 
